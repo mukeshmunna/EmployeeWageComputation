0094ee21d79a2e4f5a05e327966afd517a4552f6
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-19 16:24:50 +0530

    [Mukesh] add : created for company wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,8 +5,10 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Employee Wages");
-        EmployeeAttendance employeeAttendance = new EmployeeAttendance();
-        employeeAttendance.CalculateEmpWage();
+        EmployeeAttendance applewagess = new EmployeeAttendance("Apple", 100, 20, 100);
+        applewagess.CalculateEmpWage();
 
+        EmployeeAttendance samsumgwages = new EmployeeAttendance("Samsumg", 80, 25, 150);
+        samsumgwages.CalculateEmpWage();
     }
 }
